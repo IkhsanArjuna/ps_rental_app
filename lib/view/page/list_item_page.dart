9f70e5fa36554cb7cfcfb696fd6499dcf64fcc9c
IkhsanArjuna <arjunayuharsyah@gmail.com>
2024-06-29 12:16:34 +0700

    list item page

---
+++ lib/view/page/list_item_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ListItemPage extends StatelessWidget {
+  const ListItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    
+    );
+  }
+}
