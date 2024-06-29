683ecd1d1c84fcd4dd01a2c94f5be66d95ba03e5
yudhosaktirama <yudhosakti89@gmail.com>
2024-06-29 10:03:53 +0700

    make auth data

---
+++ lib/models/user_model.dart
@@ -0,0 +1,41 @@
+class UserModel {
+  final int idUser;
+  final String name;
+  final String email;
+  final String avatar;
+  final String role;
+  final int rentCount;
+  final int bookmarkCount;
+
+  UserModel(
+      {required this.avatar,
+      required this.bookmarkCount,
+      required this.email,
+      required this.idUser,
+      required this.name,
+      required this.rentCount,
+      required this.role});
+
+  factory UserModel.getDataFromJSON(Map<String, dynamic> json) {
+    return UserModel(
+        avatar: json['avatar'] ?? '',
+        bookmarkCount: json['bookmark'] ?? 0,
+        email: json['email'] ?? '',
+        idUser: json['id_user'] ?? 0,
+        name: json['name'] ?? '',
+        rentCount: json['rent'] ?? 0,
+        role: json['role'] ?? '');
+  }
+
+  Map<String, dynamic> toJSON() {
+    return {
+      "id_user": idUser,
+      "name": name,
+      "email": email,
+      "avatar": avatar,
+      "role": role,
+      "rent": rentCount,
+      "bookmark": bookmarkCount
+    };
+  }
+}
