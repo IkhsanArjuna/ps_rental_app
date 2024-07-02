a85db7d3c88cb6f9217f68832743638e4d33d885
yudhosaktirama <yudhosakti89@gmail.com>
2024-07-02 11:32:44 +0700

    Navigator detail Item

---
+++ lib/view/page/detail_item_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DetailItemPage extends StatelessWidget {
+  const DetailItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("Test"),
+    );
+  }
+}
--- lib/view/page/list_item_page.dart
+++ lib/view/page/list_item_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ps_rental_app/view/page/detail_item_page.dart';
 
 
 class ListItemPage extends StatelessWidget {
@@ -39,45 +40,48 @@ class ListItemPage extends StatelessWidget {
            horizontal: MediaQuery.of(context).size.width * 0.02,
             vertical: MediaQuery.of(context).size.height * 0.01,
         ),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.2,
-          color: Color.fromRGBO(14, 19, 31, 1),
-
-          child: Row(children: [
-            Padding(
-              padding:  EdgeInsets.symmetric(
-            horizontal: MediaQuery.of(context).size.width * 0.018,
-            vertical: MediaQuery.of(context).size.height * 0.01,
-              ),
-              child: Container(
-                width: MediaQuery.of(context).size.width*0.4,
-                height: MediaQuery.of(context).size.height,
-                decoration: BoxDecoration(
-                  color:Color.fromRGBO(217, 217, 217, 1),
-                  borderRadius: BorderRadius.all(Radius.circular(15))
+        child: GestureDetector(
+          onTap: () => Navigator.push(context,MaterialPageRoute(builder: (context) => DetailItemPage(),)),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.2,
+            color: Color.fromRGBO(14, 19, 31, 1),
+          
+            child: Row(children: [
+              Padding(
+                padding:  EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.018,
+              vertical: MediaQuery.of(context).size.height * 0.01,
                 ),
-              
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: Color.fromRGBO(14, 19, 31, 1),
-
-                child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment:CrossAxisAlignment.start,
-                children: [
-                  Text("PS5 ULtimate Game Experience",style: GoogleFonts.poppins(
-                      color:Colors.white ,fontSize: 12),),
-                  Text("Rp 250.000",style: GoogleFonts.poppins(
-                      color:Colors.white ,fontSize: 12),),
-                  Text("",style: GoogleFonts.poppins(
-                      color:Colors.white ,fontSize: 6),),
-                ],),
+                child: Container(
+                  width: MediaQuery.of(context).size.width*0.4,
+                  height: MediaQuery.of(context).size.height,
+                  decoration: BoxDecoration(
+                    color:Color.fromRGBO(217, 217, 217, 1),
+                    borderRadius: BorderRadius.all(Radius.circular(15))
+                  ),
                 
-                
+                ),
               ),
-            )
-          ],),
+              Expanded(
+                child: Container(
+                  color: Color.fromRGBO(14, 19, 31, 1),
+          
+                  child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment:CrossAxisAlignment.start,
+                  children: [
+                    Text("PS5 ULtimate Game Experience",style: GoogleFonts.poppins(
+                        color:Colors.white ,fontSize: 12),),
+                    Text("Rp 250.000",style: GoogleFonts.poppins(
+                        color:Colors.white ,fontSize: 12),),
+                    Text("",style: GoogleFonts.poppins(
+                        color:Colors.white ,fontSize: 6),),
+                  ],),
+                  
+                  
+                ),
+              )
+            ],),
+          ),
         ),
 
       );
